aa3a84de3a3f0a7e1391e030a7256fb7fee357e5
Hasan Jahangir Chowdhury <hasan.jahangir@akijfood.com>
2024-08-18 17:24:03 +0600

    image initialized

--- lib/utils/image_constant.dart
+++ lib/utils/image_constant.dart
@@ -3,5 +3,34 @@ class ImageConstant {
   static String imagePath = 'assets/images';
 
   //Images
-  static String accountImg = '$imagePath/account.svg';
+  static String accountImg = '$imagePath/activity.svg';
+  static String arrowImg = '$imagePath/arrow.svg';
+  static String bannerImg = '$imagePath/banner_image.svg';
+  static String bellDarkImg = '$imagePath/bell_dark.svg';
+  static String bellImg = '$imagePath/bell.svg';
+  static String body1Img = '$imagePath/body1.svg';
+  static String body2Img = '$imagePath/body2.svg';
+  static String body3Img = '$imagePath/body3.svg';
+  static String body4Img = '$imagePath/body4.svg';
+  static String body5Img = '$imagePath/body5.svg';
+  static String body6Img = '$imagePath/body6.svg';
+  static String cyclingImg = '$imagePath/cycling.svg';
+  static String documentImg = '$imagePath/document.svg';
+  static String fireImg = '$imagePath/fire.svg';
+  static String heartImg = '$imagePath/heart.svg';
+  static String homeImg = '$imagePath/home.svg';
+  static String joggingImg = '$imagePath/jogging.svg';
+  static String locationImg = '$imagePath/location.svg';
+  static String logOutImg = '$imagePath/log_out.svg';
+  static String moonImg = '$imagePath/moon.svg';
+  static String personDarkImg = '$imagePath/person_dark.svg';
+  static String playImg = '$imagePath/play.svg';
+  static String profileAvatarImg = '$imagePath/profile_avatar.svg';
+  static String profileImg = '$imagePath/profile.svg';
+  static String shoeImg = '$imagePath/shoe.svg';
+  static String statisticsImg = '$imagePath/statistics.svg';
+  static String sunImg = '$imagePath/sun.svg';
+  static String tikImg = '$imagePath/tik.svg';
+  static String timerImg = '$imagePath/timer.svg';
+  static String yogaImg = '$imagePath/yoga.svg';
 }
